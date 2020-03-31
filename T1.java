fc5d2e03fb104b83c68cd0047140df048c34b801
jiam <jiaminqiangx@163.com>
2020-03-31 22:36:56 +0800

    单链表

---
+++ T1.java
@@ -0,0 +1,19 @@
+public class T1{
+    public static void main(String[] args){
+        switch1();
+
+    }
+    public static void switch1(){
+        char v = 'B';
+        switch (v){
+        case ('A'):
+            System.out.println(10);
+        case ('C'):
+            System.out.println(20);
+       
+
+
+    }
+}
+
+}
---
+++ t.py
@@ -0,0 +1,5 @@
+def test():
+    print(aaa)
+
+setattr(test,'a',1)
+print(test.__name__)
